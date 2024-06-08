f9740fd006a3dac6b9c6b63aeea7858faafb1dfb
bailoun <mehdi.bailoun@outlook.com>
2024-06-08 04:16:47 +0300

    Fixed image going out of frame on smaller screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,28 @@
-'use client'
-
 import PokeNavBar from "@/components/pokeNavBarComp";
-import { Spinner, Button } from "react-bootstrap"; // Import Button
+import { Spinner, Button } from "react-bootstrap";
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 import Row from 'react-bootstrap/Row';
-import Link from 'next/link'; // Import Link for navigation
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <>
       <PokeNavBar />
-      <div>
-        <Container>
-          <Row className="justify-content-center">
-            <Col className="text-center" style={{ marginTop: '7rem' }}>
-              <Image src="/images/pokedex-label.png" rounded />
-                <div style={{ marginTop: '1.5rem' }}>
-                  <h1>Discover the world of Pokémon with my Pokédex...</h1>
-                </div>
-                <div style={{ marginTop: '3rem' }}>
-                  <Link href="/pokemonlist" passHref>
-                    <Button variant="warning" size="lg">
-                      Go to Pokémon Page!
-                    </Button>
-                  </Link>
-                </div>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+      <Container>
+        <Row className="justify-content-center">
+          <Col xs={12} className="text-center" style={{ marginTop: '7rem' }}>
+            <Image src="/images/pokedex-label.png" rounded style={{ maxWidth: '100%', maxHeight: '100%' }} />
+            <h1 style={{ marginTop: '1.5rem' }}>Discover the world of Pokémon with my Pokédex...</h1>
+            <Link href="/pokemonlist" passHref>
+              <Button variant="warning" size="lg" style={{ marginTop: '3rem' }}>
+                Go to Pokémon Page!
+              </Button>
+            </Link>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
