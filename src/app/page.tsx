cb55fadb5bb7c829dadeefb205615db3e10eba42
bailoun <mehdi.bailoun@outlook.com>
2024-06-08 03:33:54 +0300

    Added a homepage, and modified how a pokemons attributes are listed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,32 @@
 'use client'
-import PokemonsComp from "@/components/pokemonsComp";
-import PokeNavBar from "@/components/pokeNavBarComp";
-import PokemonCard from "@/model/pokemonCard";
-import { useEffect, useState } from "react";
-import { Container, Row, Spinner } from "react-bootstrap";
 
+import PokeNavBar from "@/components/pokeNavBarComp";
+import { Spinner, Button } from "react-bootstrap"; // Import Button
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+import Link from 'next/link'; // Import Link for navigation
 
 export default function Home() {
-
-
- const [pokemons, setPokemons] = useState<PokemonCard[]>();
-
-
- useEffect(() => {
-   const fetchData = async () => {
-     const resp = await fetch('/api/pokemon');
-     if (resp.ok) {
-       const pokemons: PokemonCard[] = (await resp.json()).items;
-       console.log(pokemons);
-       setPokemons(pokemons);
-     }
-   };
-
-
-   fetchData()
-     // Making sure to log errors on the console
-     .catch(error => {
-       console.error(error);
-     });
- }, []);
-
-
- return (
-   <>
-     <PokeNavBar></PokeNavBar>
-     {pokemons ?
-       <PokemonsComp pokemons={pokemons}></PokemonsComp> :
-       <Container>
-         <Row className="justify-content-md-center p-2">
-           <Spinner className='p-2' animation='border' role='status' />
-         </Row>
-         <Row className="justify-content-md-center p-2">
-           Loading Pokémons...
-         </Row>
-       </Container>
-     }
-   </>
- );
+  return (
+    <>
+      <PokeNavBar />
+      <div>
+        <Container>
+          <Row className="justify-content-center">
+            <Col xs={6} md={6} lg={6} xl={6} className="text-center" style={{ marginTop: '7rem' }}>
+              <Image src="/images/pokedex-label.png" rounded />
+              <h1>Discover the world of Pokémon with my Pokedex...</h1>
+              <Link href="/pokemon_list" passHref>
+                <Button variant="warning" style={{ marginTop: '1rem' }}>
+                  Go to Pokémon Page!
+                </Button>
+              </Link>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
 }
--- src/components/pokeNavBarComp.tsx
+++ src/components/pokeNavBarComp.tsx
@@ -1,23 +1,22 @@
 "use client";
 
-
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Container } from 'react-bootstrap';
 
-
 export default function PokeNavBarComp() {
    return (
        <>
-           <Navbar bg="dark" data-bs-theme="dark">
+           <Navbar bg="dark" data-bs-theme="dark" sticky="top" >
                <Container>
                    <Navbar.Brand href='/'>Pokédex</Navbar.Brand>
                    <Nav className="me-auto">
                        <Nav.Link href='/'>Home</Nav.Link>
-                       <Nav.Link href='/'>Pokémon</Nav.Link>
+                       <Nav.Link href='/pokemon_list'>Pokémon</Nav.Link>
                    </Nav>
                </Container>
            </Navbar>
        </>
    );
 }
+
